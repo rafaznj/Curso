a610cae8d0fd7d42328740c4324dbfd39234c8e2
rafaznj <rafaelsenabarretp@gmail.com>
2025-02-17 15:43:42 -0300

    Projeto add

---
+++ Desconstruindo arrays e objetos/script.js
@@ -0,0 +1,15 @@
+let pessoa = {
+    nome: "Rafael", sobrenome: "Sena", empresa: "CEPEDI", cargo: "Estágiario"
+}; 
+
+// // console.log(pessoa.nome);
+// // console.log(pessoa.cargo);
+
+// const { nome, cargo } = pessoa;
+
+// console.log(pessoa.nome);
+// console.log(pessoa.cargo); 
+
+let nome = ["Roberto", "Jorge", "Mauricio"];
+
+let [primeiro, segundo] = nome;
