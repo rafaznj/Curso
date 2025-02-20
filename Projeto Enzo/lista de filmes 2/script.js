fe09b9a589c96abb027f7e891b4e1a7f14bd1e16
rafaznj <rafaelsenabarretp@gmail.com>
2025-02-20 16:37:30 -0300

    tentado incrementar

--- Projeto Enzo/lista de filmes 2/script.js
+++ Projeto Enzo/lista de filmes 2/script.js
@@ -1,16 +1,14 @@
+// ao pesquisar dnv um filme, ele devolveo o filme anterior pesquisado 
+
 let listafilmes = []
+let filmesiguais = [];
+let historico = []; // recebe oq foi digitado em pesquisa
 
 let listagem = document.getElementById("listagem");
-
-let pesquisas = document.getElementById("pesquisas")
-
 let listaFilmesInterface = document.getElementById("ulFilmes")
+let historicoFilmesInteface = document.getElementById("historicoFilmes") // interface historico de pesquisas
 
 let quant = 0;
-
-let filmesiguais = [];
-
-let historico = [];
 
 function adicionarFilme() {
     let filme = {
@@ -25,30 +23,31 @@ function adicionarFilme() {
 
 function pesquisarFilme() {
     let pesquisa = prompt("");
+    
+    for(i = 0; i < historico.length; i++){
+        historico[i].push(pesquisa)
+    }
 
     for (i = 0; i < listafilmes.length; i++) {
         if (listafilmes[i].nome == pesquisa) {
             quant++
             filmesiguais.push(listafilmes[i])
-
-            // if (pesquisa && !historico[i].includes(pesquisa)) {
-            //     historico[i].push(pesquisa);
-            // }
         }
-    } 
+    }
 
     listagem.innerText = `Foram encontrados (${quant}) filmes`
-    
+
     for (i = 0; i < filmesiguais.length; i++) {
         let liFilme = document.createElement("li")
         liFilme.innerHTML = `Nome: ${filmesiguais[i].nome}<br> Diretor: ${filmesiguais[i].diretor}<br> Ano: ${filmesiguais[i].ano}`
         listaFilmesInterface.appendChild(liFilme)
-        }
     }
+}
 
-    // function historicoBuscas(historico) {
-    //     pesquisas.innerHTML = `Histórico de Buscas <br> ${historico}`
-    //     for (let i = 0; i < historico.length; i++) {
-    //        pesquisas.innerHTML = `${i + 1}. ${historico[i]}<br>`
-    //     }
-    // }
+
+function historicoDePesquisas() { // historico de pesquisas 
+    historicoFilmesInteface.innerHTML = `Historico de pesquisas <br>`
+    for (i = 0; i < historico.length; i++){
+        historicoFilmesInteface.innerHTML = `${historico[i]} <br>`
+    }
+}
